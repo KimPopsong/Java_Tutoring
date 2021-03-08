022cbfdc4893ce11dd8a11dce8ea756ba018e35f
Kim DaeSeong <kimds5344@naver.com>
2021-03-08 11:44:16 +0900

    2021. 03. 08.

--- InputOutPut/Scanner1.java
+++ InputOutPut/Scanner1.java
@@ -2,7 +2,7 @@ package InputOutPut;
 
 import java.util.Scanner;
 
-public class Main {
+public class Scanner1 {
     public static void main(String[] args)
     {
         Scanner scanner = new Scanner(System.in);
