6e8903f96e0f9f6cb1431d59c1f3d1aa6f54f884
Kim DaeSeong <kimds5344@naver.com>
2021-03-19 17:58:43 +0900

    2021. 03. 19.

--- InputOutPut/Scanner2.java
+++ InputOutPut/Scanner2.java
@@ -1,7 +1,7 @@
 package InputOutPut;
 
 import java.util.Scanner;
-import java.util.*;
+import java.util.*;  // *은 java.util에 있는 모든 모듈을 포함한다는 의미
 
 class Scanner2 {
     public static void main(String[] args) {
