5774a158ad1e4206507e97594ac98f80b793ca60
Kim DaeSeong <kimds5344@naver.com>
2021-03-19 16:02:43 +0900

    2021. 03. 19.

--- InputOutPut/Print1.java
+++ InputOutPut/Print1.java
@@ -1,15 +1,7 @@
 package InputOutPut;
 
-public class Print1{
-    public static void main(String[] args) { //psvm + tab
-        int n = 10; //Ctrl + Shift + Enter
-        String a = "Hello World!";
-        String b = "Hi";
-
-        System.out.println("Hello World!"); //Intelli J -> sout + tab, eclipse -> syso + ctrl + space
-        System.out.println(n);
-        System.out.println(a);
-
-        System.out.println(a + b + " "+ n);
+public class Print1 {
+    public static void main(String[] args) {  // psvm + tab
+        System.out.println("Hello World!");  // Intelli J -> sout + tab, eclipse -> syso + ctrl + space
     }
 }
