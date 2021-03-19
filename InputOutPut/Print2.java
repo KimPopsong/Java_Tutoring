b234f68f73e3e5f72887e499593acb5244cde610
Kim DaeSeong <kimds5344@naver.com>
2021-03-19 16:03:08 +0900

    2021. 03. 19.

---
+++ InputOutPut/Print2.java
@@ -0,0 +1,15 @@
+package InputOutPut;
+
+public class Print2 {
+    public static void main(String[] args) {
+        int n = 10; //Ctrl + Shift + Enter
+        String a = "Hello World!";
+        String b = "Hi";
+
+        System.out.println("Hello World!");
+        System.out.println(n);
+        System.out.println(a);
+
+        System.out.println(a + b + " "+ n);
+    }
+}
