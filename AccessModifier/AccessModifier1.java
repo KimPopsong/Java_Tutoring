1dd112dcaa23dc72ad86fbb9e40e7960a0b8aeb2
Kim DaeSeong <kimds5344@naver.com>
2021-03-22 14:15:01 +0900

    2021. 03. 22.

--- AccessModifier/AccessModifier1.java
+++ AccessModifier/AccessModifier1.java
@@ -1,15 +1,26 @@
 package AccessModifier;
 
+class AmSample1{
+    public int d = 3;
+    protected int e = 4;
+    private int f = 5;
+}
+
 public class AccessModifier1 {
-    public int a = 3;
-    protected int b = 4;
-    private int c = 6;
+    public int a = 0;
+    protected int b = 1;
+    private int c = 2;
 
     public static void main(String[] args) {
         AccessModifier1 ac1 = new AccessModifier1();
+        AmSample1 amSample1 = new AmSample1();
 
         System.out.println(ac1.a);
         System.out.println(ac1.b);
         System.out.println(ac1.c);
+
+        System.out.println(amSample1.d);
+        System.out.println(amSample1.e);
+        //System.out.println(amSample1.f);  // f는 private기 때문에 AccessModifier1에서 접근 불가
     }
 }
