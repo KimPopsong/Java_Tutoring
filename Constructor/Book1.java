ff5e6c99a18b7a8e480d23cef84d3d766987b718
Kim DaeSeong <kimds5344@naver.com>
2021-03-22 14:47:06 +0900

    2021. 03. 22.

--- Constructor/Book1.java
+++ Constructor/Book1.java
@@ -4,33 +4,20 @@ public class Book1 {
     String title;
     String author;
 
-    void show() {
-        System.out.println(title + " " + author);
+    public Book1(String t) { // 생성자
+        title = t;
+        author = "작자미상";
     }
 
-    public Book1() {
-        this("", "");
-        System.out.println("Constructor.Book 생성자 끝");
-    }
-
-    public Book1(String title) {
-        this(title, "작자미상");
-        System.out.println("Constructor.Book(title) 생성자 호출");
-    }
-
-    public Book1(String title, String author) {
-        this.title = title;
-        this.author = author;
-        System.out.println("Constructor.Book(title, author) 생성자 호출");
+    public Book1(String t, String a) { // 생성자
+        title = t;
+        author = a;
     }
 
     public static void main(String [] args) {
         Book1 littlePrince = new Book1("어린왕자", "생텍쥐페리");
         Book1 loveStory = new Book1("춘향전");
-        Book1 bible = new Book1();
-
-        littlePrince.show();
-        loveStory.show();
-        bible.show();
+        System.out.println(littlePrince.title + " " + littlePrince.author);
+        System.out.println(loveStory.title + " " + loveStory.author);
     }
 }
