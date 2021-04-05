2a4044d81f85daabda540eba27290a033689978a
KimPopsong <kimds5344@naver.com>
2021-04-05 19:56:24 +0900

    2021. 04. 05.

--- Interface/Interface1.java
+++ Interface/Interface1.java
@@ -4,7 +4,6 @@ interface Itf1{  // 인터페이스의 메소드와 상수는 기본적으로 pu
     public double PI = 3.14;  // 인터페이스는 필드 선언 불가. 상수만 가능.
 
     public void Print1();  // 인터페이스의 모든 메소드는 추상 메소드
-
     void Print2();  // public 생략
 }
 
